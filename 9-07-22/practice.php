f366ef6d8c54f1add6b7fdfabde34dc7a3d175c8
abirqb <abir.khan290@gmail.com>
2022-09-07 16:18:28 +0600

    new changes

--- 9-07-22/practice.php
+++ 9-07-22/practice.php
@@ -5,8 +5,8 @@
     <title>WEB 3.0</title>
   </head>
   <body>
+    <h1><u>Practicing php</u></h1>
     <?php
-
     $console = 54;
     $laser = "His Name: ";
 
@@ -24,8 +24,22 @@
     }
     familyName("Abir", "1997");
     familyName("Anik", "1996");
-    familyName("Abhi", "1998"); //changes is made 
+    familyName("Abhi", "1998"); //changes is made
     //PHP ArrayAccess
+
+    $cars = array("HL1", "CM2", "HL3", "HL4");
+    $cars[0] = "Volvo";
+    $cars[1] = "BMW";
+    $cars[2] = "Toyota";
+
+    // echo "$cars[2]";
+    $arrlength = count($cars);
+
+    //LOOP trying
+    for($var = 0; $var < $arrlength; $var++){
+      echo $cars[$var];
+      printf("<br>");
+    }
 
 
     //</br>
