c454ad6ea3a397d4576c8f6ec201c4f317a9a4ed
abirqb <abir.khan290@gmail.com>
2022-09-07 15:49:33 +0600

    first line code

---
+++ 9-07-22/new.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+    <h1><u>SySCON Group</u></h1>
+    <h3>LARAVEL</h3>
+    <?php
+    echo ".. ";
+     ?>
+  </body>
+</html>
---
+++ 9-07-22/practice.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>WEB 3.0</title>
+  </head>
+  <body>
+    <?php
+
+    $console = 54;
+    $laser = "His Name: ";
+
+    echo "Digitas = $console <br>";
+    printf("$laser is Abir");
+
+    function writeCO()
+    {
+      echo "<br>hi,am a function,mate<br>";
+    }
+
+    writeCO();
+    function familyName($fname, $year){
+      echo "$fname Khan. Born is $year <br>";
+    }
+    familyName("Abir", "1997");
+    familyName("Anik", "1996");
+    familyName("Abhi", "1998");
+    familyName("Amit", "2002");
+
+    //</br>
+     ?>
+  </body>
+</html>
